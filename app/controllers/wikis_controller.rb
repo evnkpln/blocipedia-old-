51cfd18cfd21e9a6272ab09f3c7aedbc52f7224e
Evan Kaplan <evnkpln@gmail.com>
2017-07-03 17:29:32 +0000

    Configure bootstrap and add index and new actions.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,4 +1,6 @@
 class WikisController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @wikis = Wiki.all
   end
@@ -8,8 +10,42 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
   end
-
+  
+  def create
+    @wiki = Wiki.build(wiki_params)
+    @wiki.user = current_user
+    
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
+  end
+  
   def edit
+    @wiki = Wiki.new
+  end
+  
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+    
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :show
+    end
+  end
+  
+  private
+  
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
   end
 end
